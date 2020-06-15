90321ddf1f2db5cce70dfbf4c5b71ba082ff90ee
scissorhands <edward_beats@hotmail.com>
2020-06-15 17:00:27 -0500

    welcome view

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,4 @@
+@extends('templates.layout')
+@section('content')
+    <h2>Welcome to my Bloggity Blog</h2>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', function(){ return view('welcome'); })->name('welcome');
 Route::get('/home', function(){ return view('home'); })->name('home');
 Route::resource('/posts', 'PostController');
 Auth::routes();
