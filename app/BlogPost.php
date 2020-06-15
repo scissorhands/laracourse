75884484cf5c14c53f2d4ee4671c5fe8e1a4d701
scissorhands <edward_beats@hotmail.com>
2020-06-15 17:09:52 -0500

    show authors

--- app/BlogPost.php
+++ app/BlogPost.php
@@ -14,6 +14,11 @@ class BlogPost extends Model
         return $this->hasMany('App\Comment');
     }
 
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
     public static function boot(){
     	parent::boot();
 
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,6 +4,12 @@
             @forelse ($posts as $post)
                 <div class="col-sm-12" style="padding-top: 28px">
                     <h3><a href="{{ route('posts.show', ['post'=>$post->id]) }}">{{ $post->title }}</a></h3>
+
+                    <p class="text-muted">
+                        Added {{ $post->created_at->diffForHumans() }}
+                        by {{ $post->user->name }}
+                    </p>
+                    
                     @if($post->comments_count)
                         <p>{{ $post->comments_count }} comments</p>
                     @else
