89251a05bc7592cdfca795961c5f87ac31d5ff77
Edward Scissorhands <edward_beats@hotmail.com>
2020-08-28 15:09:08 -0500

    email after comment

--- app/Http/Controllers/PostCommentController.php
+++ app/Http/Controllers/PostCommentController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\BlogPost;
 use Illuminate\Http\Request;
 use App\Http\Requests\StoreComment;
+use App\Mail\CommentPosted;
+use Illuminate\Support\Facades\Mail;
 
 class PostCommentController extends Controller
 {
@@ -13,10 +15,15 @@ class PostCommentController extends Controller
     }
 
     public function store(BlogPost $post, StoreComment $request){
-        $post->comments()->create([
+        $comment = $post->comments()->create([
             'content'  => $request->input('content'),
             'user_id' => $request->user()->id
         ]);
+
+        Mail::to($post->user)->send(
+            new CommentPosted($comment)
+        );
+
         $request->session()->flash('status', 'Comment was created!');
         return redirect()->back();
     }
