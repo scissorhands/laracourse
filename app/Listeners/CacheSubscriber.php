424cb3b00e95a6d791b36765fa4bce9166bd3045
Edward Scissorhands <edward_beats@hotmail.com>
2020-09-01 17:07:13 -0500

    add listener to log cache hit and miss

---
+++ app/Listeners/CacheSubscriber.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Cache\Events\CacheHit;
+use Illuminate\Cache\Events\CacheMissed;
+use Illuminate\Support\Facades\Log;
+
+class CacheSubscriber
+{
+    /**
+     * Handle the event.
+     *
+     * @param  object  $event
+     * @return void
+     */
+    public function handleCacheHit(CacheHit $event)
+    {
+        Log::info("{$event->key} cache hit");
+    }
+
+    public function handleCacheMissed(CacheMissed $event){
+        Log::info("{$event->key} cache miss");
+    }
+
+    public function subscribe($events){
+        $events->listen(
+            CacheHit::class,
+            'App\Listeners\CacheSubscriber@handleCacheHit'
+        );
+
+        $events->listen(
+            CacheMissed::class,
+            'App\Listeners\CacheSubscriber@handleCacheMissed'
+        );
+    }
+}
